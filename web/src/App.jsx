20ed30452c2ea4e00adcaee1a77386f1cceaf13f
TypicalUsername-ai <mati.domalewski@gmail.com>
2023-11-07 10:51:00 +0100

    FEAT: add tailwindcss and daisyui

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React haha</h1>
-      <div className="card">
+      <div className="w-10 card btn">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
