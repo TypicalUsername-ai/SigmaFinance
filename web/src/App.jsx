1611f0e2aa087da1c7fbf16c329022ac3fbde5f1
TypicalUsername-ai <mati.domalewski@gmail.com>
2023-11-08 15:39:39 +0100

    minor sorting alg improvements

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
   function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
+      const j = Math.floor(Math.random() * (array.length));
       [array[i], array[j]] = [array[j], array[i]];
     }
   }
