ad39b2320126a86281afff37311244dfdee32c7b
TypicalUsername-ai <mati.domalewski@gmail.com>
2023-11-14 22:17:20 +0100

    search results scroll\

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
             tabIndex={0}
             onChange={(e) => setQueried(coins.search(e.target.value))}  
             placeholder='Search' className='input input-bordered rounded-md' />
-          <div tabIndex={0} class="dropdown-content z-[40] p-2 bg-primary-content flex flex-col">
+          <div tabIndex={0} class="dropdown-content max-h-96 overflow-scroll z-[40] p-2 bg-primary-content flex flex-col">
             {queried.map(
               (entry) => <Link 
                 className='p-2 btn btn-ghost rounded'
