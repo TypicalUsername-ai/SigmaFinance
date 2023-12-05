4aad63b7f5f52d5005d8a013c1f173ec921b04c8
Taras Zhyhulin <zhyhulin@student.agh.edu.pl>
2023-12-05 10:14:48 +0100

    removed unnecessary test calls

--- web/src/components/UserTab.jsx
+++ web/src/components/UserTab.jsx
@@ -1,15 +1,12 @@
 import { useState, useContext, useEffect } from "react"
 import { SupabaseContext } from "../supabaseContext"
 import { Link } from "react-router-dom"
-import { getTrackedIndices, makeObjectFavourite } from "../functions/userCoins"
 
 const UserTab = () => {
   const supabase = useContext(SupabaseContext)
   const [data, setData] = useState(null)
 
   useEffect(() => {
-    getTrackedIndices(supabase)
-    makeObjectFavourite(supabase, "UAH")
     supabase.auth.getUser().then(
       data => setData(data.data.user)
     )
